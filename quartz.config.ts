227d68dcb545bd78f30c8392e97509bd7f18454e
Oskar Flygare <oskarflygare@gmail.com>
2023-09-06 14:32:52 +0200

    Fix RecentNotes layout

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Oskar's notes",
+    pageTitle: "Notes from an aspiring academic",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
